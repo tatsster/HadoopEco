df28d7d490882e9e97c8831c6f43c4a1ae5c08c7
nhanpt6 <nhanpt6@vng.com.vn>
2023-04-22 11:27:51 +0700

    Delete old files

--- conf/spark-env.sh
+++
@@ -1,6 +0,0 @@
-export SPARK_HOME=/opt/spark-3.2.0-bin-hadoop3.2
-export HADOOP_HOME=/opt/hadoop-3.3.1
-export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
-export YARN_CONF_DIR=$HADOOP_HOME/etc/hadoop
-export SPARK_DIST_CLASSPATH=$($HADOOP_HOME/bin/hadoop classpath)
-export SPARK_LOCAL_IP=localhost
